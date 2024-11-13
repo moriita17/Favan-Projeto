24c5972147f20094f01ce368fffaa7367267ecd3
Enzo <enzomoriita17@gmail.com>
2024-11-13 16:19:52 -0300

    criação CRUD usuario

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+usuarios = []
+    
+class Usuarios(BaseModel):
+    nome: str
+    username: str
+
 @app.get("/")
-def read_root():
-    return {"message": "GABRIEL PARISSOTO"}
+def home():
+    return {"Usuarios": usuarios}
+
+@app.post("/usuarios/inserir")
+def InserirUsuario(usuario: Usuarios):
+    usuarios.append ([usuario.nome, usuario.username])
+    return {"Mensagem": f"O Usuario {usuario.nome} foi cadastrado com sucesso"}
+
+@app.put("/usuarios/editar/{id}")
+def editarUsuario(id: int, usuario: Usuarios):
+    usuarios[id - 1] = [usuario.nome, usuario.username]
+    return {"Mensagem": f"O Usuario n {id} foi editado com sucesso"}
+
+@app.delete("/usuarios/apagar/{id}")
+def apagarUsuario(id: int):
+    usuarios.pop(id-1)
+    return {"Mensagem": f"O Usuario n{id} foi apagado com sucesso"}
+    
+    
+    
