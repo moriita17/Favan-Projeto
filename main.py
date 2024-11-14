fb260e7ffb15da5c9dc7b2900d501113f374a34c
Enzo <enzomoriita17@gmail.com>
2024-11-14 15:33:46 -0300

    criação CRUD produtos

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
 usuarios = []
+produtos = []
     
 class Usuarios(BaseModel):
     nome: str
     username: str
+    
+class Produto(BaseModel):
+    nome: str
+    preco: float
+    quantidade: int
 
 @app.get("/")
 def home():
@@ -27,6 +34,33 @@ def editarUsuario(id: int, usuario: Usuarios):
 def apagarUsuario(id: int):
     usuarios.pop(id-1)
     return {"Mensagem": f"O Usuario n{id} foi apagado com sucesso"}
+
+@app.get("/produtos")
+def listarProdutos():
+    return {"Produtos": produtos}
+
+@app.post("/produtos/inserir")
+def inserirProduto(produto: Produto):
+    produtos.append({
+        "nome": produto.nome,
+        "preco": produto.preco,
+        "quantidade": produto.quantidade
+    })
+    return {"Mensagem": f"O Produto {produto.nome} foi cadastrado com sucesso"}
+
+@app.put("/produtos/editar/{id}")
+def editarProduto(id: int, produto: Produto):
+    produtos[id - 1] = {
+        "nome": produto.nome,
+        "preco": produto.preco,
+        "quantidade": produto.quantidade
+    }
+    return {"Mensagem": f"O Produto n {id} foi editado com sucesso"}
+
+@app.delete("/produtos/apagar/{id}")
+def apagarProduto(id: int):
+    produtos.pop(id - 1)
+    return {"Mensagem": f"O Produto n{id} foi apagado com sucesso"}
     
     
     
